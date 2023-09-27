0d2f92b6c11cec9fcff53239bd6139e77ec0e0fc
JennerMaxim <jennersi1remaxim@gmail.com>
2023-09-27 23:44:11 +0300

    Added submit form data to google sheet functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Toggle Mobile menu
 const hamburgerMenu = document.querySelector(".hamburger");
 const closeMenuBtn = document.querySelector(".navigation");
 const navigation = document.querySelector(".navigation");
@@ -25,6 +26,30 @@ document.querySelectorAll("a").forEach((a) => {
 hamburgerMenu.addEventListener("click", openMenu)
 closeMenuBtn.addEventListener("click", closeMenu)
 
+// Submit form to google sheet
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwqSuxUuaWVxw5jZ7MqH993i4_rXP7T1gwQM5ObI1JOrxbqbBkyFxYGd6vnbstaAw4Rgg/exec'
+
+form.addEventListener('submit', e => {
+    submitButton.disabled = true
+    e.preventDefault()
+    
+    let requestBody = new FormData(form)
+
+    fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+        alert('Thank you for Joining our community! We will add you to our WhatsApp group soon', response)
+        submitButton.disabled = false
+        })
+    .catch(error => {
+    alert('Error!', error.message)
+        submitButton.disabled = false
+
+    }
+    )
+})
+
 
 // Accordion Logic
 var acc = document.getElementsByClassName("accordion");
@@ -45,24 +70,3 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-
-
-
-// const acc = document.getElementsByClassName("accordion");
-// let i;
-
-// for (i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function () {
-//         /* Toggle between adding and removing the "active" class,
-//         to highlight the button that controls the panel */
-//         this.classList.toggle("active");
-
-//         /* Toggle between hiding and showing the active panel */
-//         var panel = this.nextElementSibling;
-//         if (panel.style.display === "block") {
-//             panel.style.display = "none";
-//         } else {
-//             panel.style.display = "block";
-//         }
-//     });
-// }
